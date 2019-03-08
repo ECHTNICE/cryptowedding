55b4a248a3a7be18fd1399e092f55ee6f99233c5
pmizel <foto@enivo.de>
2019-03-08 22:46:49 +0100

    update text

--- client/src/About.tsx
+++ client/src/About.tsx
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import "./About.css";
 
 class About extends Component {
+
+     SvgWithXlink () {
+        return (
+            <svg viewBox="0 0 500 500">
+                <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+                <text width="500">
+                    <textPath xlinkHref={ "#curve" }>
+                        Marriage
+                    </textPath>
+                </text>
+            </svg>
+        );
+    }
+
   render() {
-    return <div>dwad</div>;
+    return <div>
+        {this.SvgWithXlink()}
+    </div>;
   }
 }
 
