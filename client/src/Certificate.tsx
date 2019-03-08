6aa12c3413efd03c0a2321d5eadc6c99fe1cd153
pmizel <foto@enivo.de>
2019-03-08 23:12:45 +0100

    add certification site

--- client/src/Certificate.tsx
+++ client/src/Certificate.tsx
@@ -1,21 +1,41 @@
 import React, { Component } from "react";
-import "./About.css";
+import "./Certificate.css";
 import {Grid} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper/Paper";
 
-class About extends Component {
+class Certificate extends Component {
 
+     SvgWithXlink () {
+        return (
+            <svg viewBox="0 0 500 500">
+                <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
+                <text width="500">
+                    <textPath xlinkHref={ "#curve" }>
+                        &nbsp;Marriage
+                    </textPath>
+                </text>
+            </svg>
+        );
+    }
 
   render() {
     return <div>
 
         <Grid container spacing={24}>
             <Grid item xs={12}>
-                <Paper >
-                  <h1>About</h1>
+                <Paper >xs=12</Paper>
+            </Grid>
+            <Grid item xs={2}>
+                <Paper >xs=2</Paper>
+            </Grid>
+            <Grid item xs={8}>
+                <Paper>
+                    {this.SvgWithXlink()}
                 </Paper>
             </Grid>
-
+            <Grid item xs={2}>
+                <Paper >xs=2</Paper>
+            </Grid>
 
             <Grid item xs={6}>
                 <Paper >xs=6</Paper>
@@ -44,4 +64,4 @@ class About extends Component {
   }
 }
 
-export default About;
+export default Certificate;
